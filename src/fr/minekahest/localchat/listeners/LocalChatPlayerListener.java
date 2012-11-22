13b0e029e23d2b5db071957572bade845fff6302
Likaos <Likaos@Likaos-PC>
2012-11-22 22:49:16 +0100

    Version 0.2 -Optimisation du code -Modification de la fonction de calcul des distances par celle de bukkit (un merci à PunKeel !) -Tous les messages de chat passent désormais par le plugin (même le global) -Rajout des couleurs et prefix -Rajout d'une commande World (@) pour le monde actuel

--- src/fr/minekahest/localchat/listeners/LocalChatPlayerListener.java
+++ src/fr/minekahest/localchat/listeners/LocalChatPlayerListener.java
@@ -25,16 +25,17 @@ public class LocalChatPlayerListener implements Listener {
 		if (event.isCancelled()) return;
 		
 		//Recupération des signes définit dans la configuration
-		String whispsign = plugin.getConfig().getString("whisp-sign");
-		String shoutsign = plugin.getConfig().getString("shout-sign");
-		String hrpsign = plugin.getConfig().getString("hrp-sign");
-		String globalsign = plugin.getConfig().getString("global-sign");
+		String whispSign = plugin.getConfig().getString("whisp-sign");
+		String shoutSign = plugin.getConfig().getString("shout-sign");
+		String hrpSign = plugin.getConfig().getString("hrp-sign");
+		String globalSign = plugin.getConfig().getString("global-sign");
+		String worldSign = plugin.getConfig().getString("world-sign");
 		
 		//Recuperation des radius de configuration
-		int whispradius = plugin.getConfig().getInt("whisp-radius");
-		int localradius = plugin.getConfig().getInt("local-radius");
-		int shoutradius = plugin.getConfig().getInt("shout-radius");
-		int hrpradius = plugin.getConfig().getInt("hrp-radius");
+		int whispRadius = plugin.getConfig().getInt("whisp-radius");
+		int localRadius = plugin.getConfig().getInt("local-radius");
+		int shoutRadius = plugin.getConfig().getInt("shout-radius");
+		int hrpRadius = plugin.getConfig().getInt("hrp-radius");
 		
 		//Le joueur qui parle
 		Player talkingPlayer = event.getPlayer();
@@ -45,69 +46,107 @@ public class LocalChatPlayerListener implements Listener {
 		//Et éviter les double phrases
 		event.setCancelled(true);
 		
-		if (event.getMessage().startsWith(globalsign) && globalsign != "false") {
+		//Message global
+		if (event.getMessage().startsWith(globalSign) && globalSign != "false") {		
 			//Suppresion du signe
 			msg = msg.substring(1).trim();
-			//Global on envois le message à tout le monde
-			plugin.getServer().broadcastMessage(ChatColor.DARK_GREEN+ talkingPlayer.getName()+": "+msg);
+			//Format
+			String finalColoredPrefix = preFormatMessage("global");
+			//Colorisation et envois serveur entier
+			sendAllMessage(talkingPlayer, finalColoredPrefix, msg);
+		}
+		//Message world actuel
+		else if (event.getMessage().startsWith(worldSign) && worldSign != "false") {		
+				//Suppresion du signe
+				msg = msg.substring(1).trim();
+				//Format
+				String finalColoredPrefix = preFormatMessage("world");
+				//Colorisation et envois serveur entier
+				sendWorldMessage(talkingPlayer, finalColoredPrefix, msg);			
 		}
 	
 		//Whisp
-		else if (event.getMessage().startsWith(whispsign) && whispsign != "false") {
+		else if (event.getMessage().startsWith(whispSign) && whispSign != "false") {
 			//Suppresion du signe
 			msg = msg.substring(1).trim();
+			//Format
+			String finalColoredPrefix = preFormatMessage("whisp");
 			//Verifie la distance des joueurs par rapport à la conf et leur envois le message
-			checkDistanceAndSendMessage(talkingPlayer, whispradius, msg, "Chuchotement");
+			checkDistanceAndSendMessage(talkingPlayer, whispRadius, finalColoredPrefix, msg);
 		}
 		
 		//Shout
-		else if (event.getMessage().startsWith(shoutsign) && shoutsign != "false") {
+		else if (event.getMessage().startsWith(shoutSign) && shoutSign != "false") {
 			//Suppresion du signe
 			msg = msg.substring(1).trim();
+			//Format
+			String finalColoredPrefix = preFormatMessage("shout");
 			//Verifie la distance des joueurs par rapport à la conf et leur envois le message
-			checkDistanceAndSendMessage(talkingPlayer, shoutradius, msg, "Cri");			
+			checkDistanceAndSendMessage(talkingPlayer, shoutRadius, finalColoredPrefix, msg);			
 		}
 		
 		//Hors-roleplay
-		else if (event.getMessage().startsWith(hrpsign) && hrpsign != "false") {
+		else if (event.getMessage().startsWith(hrpSign) && hrpSign != "false") {
 			//Suppresion du signe
 			msg = msg.substring(1).trim();
+			//Format
+			String finalColoredPrefix = preFormatMessage("hrp");
 			//Verifie la distance des joueurs par rapport à la conf et leur envois le message
-			checkDistanceAndSendMessage(talkingPlayer, hrpradius, msg, "Hrp");			
+			checkDistanceAndSendMessage(talkingPlayer, hrpRadius, finalColoredPrefix, msg);		
 		}
-				
-		//Local si aucun signe
+		
+		//Local si aucun sign
 		else {
+			String finalColoredPrefix = preFormatMessage("local");
 			//Verifie la distance des joueurs par rapport à la conf et leur envois le message
-			checkDistanceAndSendMessage(talkingPlayer, localradius, msg, "false");			
+			checkDistanceAndSendMessage(talkingPlayer, localRadius, finalColoredPrefix, msg);			
 		}				
 	}
 	
+	//Remplacement du broadcast serveur par un maison
+	private void sendAllMessage(Player talkingPlayer, String finalColoredPrefix, String msg) {
+		for (Player p : plugin.getServer().getOnlinePlayers()) {
+			p.sendMessage(finalColoredPrefix+talkingPlayer.getName()+": "+msg);
+			}
+		}
 	
-	//Un peu de math
-	public void checkDistanceAndSendMessage(Player player, Integer radius, String message, String prefix) {
+	//Broadcast par World (pull PunKeel modifié :p)
+	protected void sendWorldMessage(Player talkingPlayer, String finalColoredPrefix, String msg) {
+		//Boucle de vérification de joueurs sur le même monde
+		for (Player p : plugin.getServer().getOnlinePlayers()) {
+				if (p.getWorld() == talkingPlayer.getWorld()) {
+					p.sendMessage(finalColoredPrefix+talkingPlayer.getName()+": "+msg);
+				}
+		}
+}
 		
-		for (Player listeningPlayer : plugin.getServer().getOnlinePlayers()) {		
-			//Calcul des distances
+	//Un peu de formattage
+	public String preFormatMessage (String chatType) {
+		String prefix = plugin.getConfig().getString(chatType+"-prefix");
+		String color = plugin.getConfig().getString(chatType+"-color");
+		String coloredPrefix;
+		//Si pas de prefix on va faire une petite exeption
+		if (prefix != "false") {
+			coloredPrefix = ChatColor.translateAlternateColorCodes('&', color) + prefix + " ";
+		} else {
+			coloredPrefix = ChatColor.translateAlternateColorCodes('&', color);
+		}
+		return coloredPrefix;
+	}
+		
+	//Calcul des distances et envois
+	public void checkDistanceAndSendMessage(Player player, Integer radius, String prefix, String message) {
+		
+		for (Player listeningPlayer : plugin.getServer().getOnlinePlayers()) {
+			//Positions des 2 joueurs testés
 			org.bukkit.Location pLoc = listeningPlayer.getLocation();
 			org.bukkit.Location sLoc = player.getLocation();
-	        int dx = sLoc.getBlockX() - pLoc.getBlockX();
-	        int dz = sLoc.getBlockZ() - pLoc.getBlockZ();
-	        dx = dx * dx;
-	        dz = dz * dz;
-	        int d = (int) Math.sqrt(dx + dz);
-	        //Si distance = ok envois, sinon pas de messages :'(
-        	if (d <= radius)
-        	
-        	//Petit fix pour le local
-        		if (prefix != "false") {
-        	listeningPlayer.sendMessage("("+prefix+") "+player.getDisplayName()+": "+message);
-        		} else {
-        	listeningPlayer.sendMessage(player.getDisplayName()+": "+message);
-        		}
+	        //Si distance = ok envois, sinon pas de messages	        
+	        if(sLoc.distance(pLoc) <= radius)     
+	        //Envois du message
+        	listeningPlayer.sendMessage(prefix+player.getDisplayName()+": "+message);
 		}
 		//On log tout de même sur le serveur histoire de pas laisser l'admin dans les choux :)
-		plugin.getLogger().info(player.getName()+": "+message);
+		plugin.getLogger().info((prefix+player.getName()+": "+message).substring(2));
 	}
-
 }
